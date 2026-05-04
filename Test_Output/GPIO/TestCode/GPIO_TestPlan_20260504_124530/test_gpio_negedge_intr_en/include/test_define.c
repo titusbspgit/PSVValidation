2c1d0d439c3b7dbbb57629f02ee8e0d3b625730a
Varsh3092 <varshasomasundaram309@gmail.com>
2026-05-04 12:37:22 +0530

    Delete Test_Output/GPIO/TestCode directory

--- Test_Output/GPIO/TestCode/GPIO_TestPlan_20260504_124530/test_gpio_negedge_intr_en/include/test_define.c
+++
@@ -1,8 +0,0 @@
-#include <gpio/gpio_def.h>
-#include <gpio/gpio_offset.h>
-
-#define GPIO_CNT      32   /* pads 8..39 */
-#define EXT_PAD_CTRL  0xA0243ffcu
-#define EXT_PAD_STAT  0xA0243ff8u
-
-/* No additional constants required; addresses come from SoC headers. */
