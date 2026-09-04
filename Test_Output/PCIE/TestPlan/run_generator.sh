08a1e4d6a17b1126046af1d287d235b97d3f0265
titusbspgit <52008376+titusbspgit@users.noreply.github.com>
2026-09-04 20:10:44 +0530

    Clean up temporary run script

--- Test_Output/PCIE/TestPlan/run_generator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run the XLSX generator
-cd "$(dirname "$0")"
-pip install openpyxl 2>/dev/null
-python3 generate_PCIE_TestPlan_20250710.py
-echo "Done. Check for PCIE_TestPlan_20250710_033000.xlsx"
