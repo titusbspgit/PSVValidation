abea3059078878521c6cfd5fadb32efe0f31f943
titusbspgit <52008376+titusbspgit@users.noreply.github.com>
2026-09-16 20:50:51 +0530

    Add FV testcase: USB_FS_Device_Interrupt_Transfer_test

---
+++ Test_Output/USB/TestCode/USB_TestCode_20250917_042830/usb_fs_device_interrupt_transfer_test/usb_fs_device_interrupt_transfer_test.h
@@ -0,0 +1,13 @@
+// Author - AI Force 2.3. 17-Sep-2025 04:28 IST
+// (EMBENGG-SYSAPPS)
+
+#ifndef USB_FS_DEVICE_INTERRUPT_TRANSFER_TEST_H
+#define USB_FS_DEVICE_INTERRUPT_TRANSFER_TEST_H
+
+#include "framework.h"
+
+int usb_fs_device_interrupt_transfer_test_init(const TestsItem *cfg);
+int usb_fs_device_interrupt_transfer_test_run(const TestsItem *cfg, TestOutput *out);
+int usb_fs_device_interrupt_transfer_test_teardown(const TestsItem *cfg);
+
+#endif /* USB_FS_DEVICE_INTERRUPT_TRANSFER_TEST_H */
